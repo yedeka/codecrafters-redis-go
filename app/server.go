d5be3a2daff24e0aa2a9688893bcc7f17184032c
unknown <yogesh.edekar@gmail.com>
2025-01-12 07:16:22 -0800

    Adding infinte loop for listening at server end so that server will accept connections on 6379 port

--- app/server.go
+++ app/server.go
@@ -11,13 +11,12 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	defer l.Close()
 	for {
+		l, err := net.Listen("tcp", "0.0.0.0:6379")
+		if err != nil {
+			fmt.Println("Failed to bind to port 6379")
+			os.Exit(1)
+		}
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
