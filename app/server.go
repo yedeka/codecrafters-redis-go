c404c5204bfef6d0f957090ad354c85c1d61a7aa
unknown <yogesh.edekar@gmail.com>
2025-01-12 06:52:55 -0800

    Moving listening code to infinite loop

--- app/server.go
+++ app/server.go
@@ -14,12 +14,12 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
 	for {
+		l, err := net.Listen("tcp", "0.0.0.0:6379")
+		if err != nil {
+			fmt.Println("Failed to bind to port 6379")
+			os.Exit(1)
+		}
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
