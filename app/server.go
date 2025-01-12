69eecb8fab373f9fb664179ab32171345553efb2
unknown <yogesh.edekar@gmail.com>
2025-01-12 06:56:48 -0800

    Adding cneection handling via function

--- app/server.go
+++ app/server.go
@@ -11,20 +11,22 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	handleCommand(conn)
+}
+func handleCommand(conn net.Conn) {
+	buf := make([]byte, 1024)
 	for {
-		l, err := net.Listen("tcp", "0.0.0.0:6379")
-		if err != nil {
-			fmt.Println("Failed to bind to port 6379")
-			os.Exit(1)
-		}
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
+		_, err := conn.Read(buf)
 		_, err = conn.Write([]byte("+PONG\r\n"))
 		if err != nil {
 			return
