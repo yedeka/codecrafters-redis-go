a22c913deb0193086fe54e111cfe78e2b6c09629
unknown <yogesh.edekar@gmail.com>
2025-01-19 06:10:03 -0800

    Adding debug logs in get

--- app/command/getCommand.go
+++ app/command/getCommand.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -13,6 +14,7 @@ type GetCommand struct {
 func (get GetCommand) Execute() string {
 	var rawResponseList []ParsedResponse
 	value, ok := keyMap[get.key]
+	fmt.Printf("%+v\n", keyMap)
 	if !ok {
 		rawResponseList := append(rawResponseList, ParsedResponse{
 			Responsetype: "FAILURE",
--- app/command/setCommand.go
+++ app/command/setCommand.go
@@ -32,7 +32,6 @@ func (set SetCommand) prepareSetValue() error {
 		keyMap[set.key] = set.value
 		go func() {
 			<-set.value.timer.C
-			fmt.Println("Expiring timer")
 			delete(keyMap, set.key)
 		}()
 		return nil
