ede80b5316c5a66e78bc49fc58be6358fd5ac3c7
unknown <yogesh.edekar@gmail.com>
2025-01-19 08:03:16 -0800

    Debug messages added for connection acceptance

--- app/server.go
+++ app/server.go
@@ -22,7 +22,7 @@ func main() {
 	var listeningAddress strings.Builder
 	listeningAddress.WriteString(defaultAddress)
 	listeningAddress.WriteString(strconv.Itoa(portFlag))
-	fmt.Printf("Listening address %v\n", listeningAddress)
+	fmt.Printf("Listening address %v\n", listeningAddress.String())
 	l, err := net.Listen("tcp", listeningAddress.String())
 
 	if err != nil {
