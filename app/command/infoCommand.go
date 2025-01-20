8062df9ff59c9152cf6df23ddb2ffb8d2a9788e8
unknown <yogesh.edekar@gmail.com>
2025-01-20 05:23:46 -0800

    Adding logic for InfoCommand

--- app/command/infoCommand.go
+++ app/command/infoCommand.go
@@ -1,5 +1,51 @@
 package command
 
+import (
+	"strconv"
+	"strings"
+)
+
 type InfoCommand struct {
 	arguments []string
+}
+
+func handleArgs(args []string) []ParsedResponse {
+	responseList := []ParsedResponse{}
+	for _, arg := range args {
+		if strings.ToLower(arg) == "info" {
+			responseString := "role:master"
+			responseList = append(responseList, ParsedResponse{
+				Responsetype: "LENGTH",
+				ResponseData: strconv.Itoa(len(responseString)),
+			})
+			responseList = append(responseList, ParsedResponse{
+				Responsetype: "DATA",
+				ResponseData: responseString,
+			})
+		}
+	}
+	return responseList
+}
+
+func (info InfoCommand) Execute() string {
+	return info.FormatOutput(handleArgs(info.arguments))
+}
+
+func (info InfoCommand) FormatOutput(rawResponseList []ParsedResponse) string {
+	var commandResponse strings.Builder
+	for _, rawResponse := range rawResponseList {
+		if rawResponse.Responsetype == "LENGTH" {
+			writeResponse(lengthPrefix,
+				rawResponse.ResponseData,
+				terminationSequence,
+				&commandResponse)
+		} else if rawResponse.Responsetype == "DATA" {
+			writeResponse("",
+				rawResponse.ResponseData,
+				terminationSequence,
+				&commandResponse)
+		}
+	}
+
+	return commandResponse.String()
 }
